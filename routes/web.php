9efb08041d7c9a2e056e4bfef0dfa6f4328ad20a
Hoang Huong | apikey:d0851193d83d8c0dc0833e050f2c432f59dd585145eb42c2440363789246f383 <huong.ph@fsofts.com>
2025-03-17 22:30:41 +0700

    save

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,8 @@ Route::get('/', function () {
 Route::middleware('guest')->group(function () {
     Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
     Route::post('/login', [LoginController::class, 'login']);
-    Route::get('/dang-ky', [LoginController::class, 'showRegistrationForm'])->name('register');
-    Route::post('/dang-ky', [LoginController::class, 'register']);
+    Route::get('/register', [LoginController::class, 'showRegistrationForm'])->name('register');
+    Route::post('/register', [LoginController::class, 'register']);
 });
 
 Route::middleware('auth')->group(function () {
