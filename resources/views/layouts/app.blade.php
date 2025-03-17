db29c5fcad95ad55e525492819eb548f4477e2c6
Hoang Huong | apikey:d0851193d83d8c0dc0833e050f2c432f59dd585145eb42c2440363789246f383 <huong.ph@fsofts.com>
2025-03-17 22:51:48 +0700

    save

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('build/assets/app-bOPDhkW8.css') }}">
+    <script src="{{ asset('build/assets/app-DspuE8pW.js') }}"></script>
 </head>
 
 <body class="bg-gray-50 min-h-screen flex flex-col">
